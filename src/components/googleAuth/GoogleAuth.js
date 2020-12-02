8f6d8085d9c24b360156c7a145e5c6f415ef1de5
dobrynanikitich <nikitanemov1990@gmail.com>
2020-12-02 15:32:15 +0300

    feat: complete app

---
+++ src/components/googleAuth/GoogleAuth.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+
+import GoogleLogin from 'react-google-login';
+import { GoogleLogout } from 'react-google-login';
+
+import classes from './GoogleAuth.module.scss';
+import classNames from 'classnames/bind';
+
+let cx = classNames.bind(classes);
+
+const GoogleAuth = ({ changeState, isLogin }) => {
+    const [accountImage, setAccountImage] = useState('');
+
+    const className = cx({
+        LoginGoogle: true,
+    })
+
+    const responseGoogle = (response) => {
+        changeState('isLogin', true);
+        setAccountImage(response.profileObj.imageUrl);
+    }
+
+    const logout = () => {
+        changeState('isLogin', false);
+    }
+
+    return (
+        <div className={classes.LoginWrapper}>
+        {
+            isLogin ?
+                <>
+                    <div className={classes.AccountImg}>
+                        <img alt='account_img' src={accountImage} />
+                    </div>
+                    <GoogleLogout
+                        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                        buttonText="Logout"
+                        onLogoutSuccess={logout}
+                        className={className}
+                    />
+                </> :
+                <GoogleLogin
+                    clientId="742036064829-dpcmuogppip94tj7gh0hs3u0qgbete2m.apps.googleusercontent.com"
+                    buttonText="Login"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    cookiePolicy={'single_host_origin'}
+                    className={className}
+                />
+        }
+    </div>
+    )
+}
+
+export default GoogleAuth;
