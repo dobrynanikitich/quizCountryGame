ed49e8ae414030727a79bdf5e02d01bf526c2203
Mikita Nemau <nikitanemov1990@gmail.com>
2020-12-02 16:51:44 +0300

    feat: change  gameover comp

--- src/components/modals/GameOverModal.js
+++ src/components/modals/GameOverModal.js
@@ -5,7 +5,7 @@ import classes from './GameOverModal.module.scss';
 
 const gameOver = ({ currentScore, returnToMainPageHandler }) => (
     <section className={classes.GameOverWrapper}>
-        <div>Time is over</div>
+        <div>Time is over!</div>
         <div>You scored {currentScore} points</div>
         <Button click={() => returnToMainPageHandler()} buttonName={'playAgain'} />
     </section>
